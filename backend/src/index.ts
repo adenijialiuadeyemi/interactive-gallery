f17c074f54a3dde603f0c9cb1f4d8c656aacf284
Adeniji Aliu Adeyemi <adenijialiuadeyemi@gmail.com>
2025-07-06 18:38:02 +0100

    refractor: add comments to the index file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import imageRoutes from "./routes/images.route";
 
 import authRouter from "./routes/auth.route";
 import commentRouter from "./routes/comment.route";
@@ -12,19 +11,22 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Health check endpoint
 app.get("/", (_, res) => {
   res.send("API is running...");
 });
 
-//end points
+// Routes
 app.use("/api/images", imageRouter);
 app.use("/api/comments", commentRouter);
 app.use("/api/auth", authRouter);
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
